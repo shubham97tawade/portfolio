b411293abb80cd9844d3943afdd8916a91d10439
shubham97tawade <shubham.tawade97@gmail.com>
2025-06-29 14:35:45 +0100

    (home): home page main section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,35 @@
+"use client";
+
+import { motion } from 'framer-motion';
+
 export default function Home() {
   return (
-    <main className="h-screen w-screen">
-      <div className="grid place-items-center h-screen w-screen bg-stone-50">
-        <span className="text-center p-5">
-          <h3 className="text-xl md:text-4xl font-bold tracking-tight text-stone-950">WEBPAGE IS UNDER</h3>
-          <h1 className="text-3xl md:text-7xl font-bold tracking-tight text-yellow-500">CONSTRUCTION</h1>
-        </span>
+    <main className="w-full h-dvh grid grid-cols-1 grid-rows-1 place-items-center">
+      <div className="flex flex-col items-start justify-center">
+        <motion.h1 
+          initial={{ opacity: 0, y: -20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.7, ease: 'easeInOut' }}
+          className="text-4xl md:text-7xl"
+        >
+            Shubham Tawade
+        </motion.h1>
+        <motion.h5
+          initial={{ opacity: 0}}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.7, delay: 0.5, ease: 'easeInOut' }}
+          className="text-lg md:text-xl mb-12"
+        >
+            Developer.
+        </motion.h5>
+        <motion.p
+          initial={{ opacity: 0, y: 20}}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.7, delay: 0.7, ease: 'easeInOut' }}
+          className="text-base"
+        >
+            Welcome to my digital space. 🚀
+        </motion.p>
       </div>
     </main>
   );
