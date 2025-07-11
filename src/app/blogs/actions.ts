a15b8e026577b1826d6f04561be57e611f3efe75
shubham97tawade <shubham.tawade97@gmail.com>
2025-07-11 09:35:36 +0100

    (reset): portfolio project reset

--- src/app/blogs/actions.ts
+++
@@ -1,7 +0,0 @@
-'use server'
-
-import { supabase } from "../lib/dBClient";
-
-export const getBlogsPreviewData = async() => {
-    return  supabase.rpc('blogs_preview_query')
-}
--- src/app/lib/dBClient.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseKey = process.env.SUPABASE_ANON_KEY as string;
-const supabaseUrl = process.env.SUPABASE_URL as string;
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
